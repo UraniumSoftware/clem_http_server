361b92d47f3baefb7abb2e7319874cb9ccb5affb
ClemTheAlien <ClemTheAlien@proton.me>
2026-04-20 15:39:30 -0500

    followed most of first tutorial :)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,31 @@
 #include <netinet/tcp.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+int handle_client_read(int socket){
+    ssize_t n = 0;
+    char buffer[100];
+
+    for (;;){
+        memset(buffer, 0, sizeof(buffer));
+        n = read(socket, buffer, sizeof(buffer)-1);
+        if (n < 0){
+            perror("read(client);");
+            return -1;
+        }
+        else if (n == 0) {
+            printf("\nclient closed gracefully\n");
+            break;
+        }
+        printf("%s\n", buffer);
+
+    }
+    return 0;
+}
 
 int main(void){
 
@@ -21,6 +43,8 @@ int main(void){
         goto exit;
     }
 
+    printf("Socket opened :)\n");
+
     struct sockaddr_in bind_addr = {0}; /*Initializes IPv4 socket*/
     bind_addr.sin_port =  htons(420), /*Port Number listening on*/
     bind_addr.sin_family = AF_INET, /*The family of the request (IPv4)*/
@@ -33,6 +57,8 @@ int main(void){
         ret = 1;
         goto exit;
     }
+    
+    printf("Socket has a port and IP binded to it!\n");
 
     rc = listen(tcp_socket, SOMAXCONN); /*Now we are listening*/
     if (rc != 0){
@@ -41,6 +67,23 @@ int main(void){
         goto exit;
     }
 
+    int client_socket = 0; 
+
+    for (;;){ /*While true loop that loops the waiting to accept a request*/
+    client_socket = accept(tcp_socket, NULL,NULL);
+    if (client_socket < 0){
+        perror("accept();");
+        ret = 1;
+        goto exit;
+    }
+
+    printf("Socket listened and accepted a request!\n");
+    if (handle_client_read(client_socket) != 0){
+        ret = 1;
+        goto exit;
+    }
+    printf("\nYipee!\n");
+    }
     exit:
         close(tcp_socket);
         return ret;
